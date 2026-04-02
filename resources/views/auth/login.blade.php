aa9ef6e06fb0cadf8ed86bedf796055bfb2a5ed4
Bienvenue GBAGUIDI <bgbaguidi793@gmail.com>
2026-04-02 14:18:11 +0100

    V4.2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,81 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - SAV Mikem</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .cb-slideshow,
+        .cb-slideshow:after {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+            top: 0px;
+            left: 0px;
+            z-index: 0;
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        .cb-slideshow:after {
+            content: '';
+            background: rgba(0,0,0,0.5); /* Masque sombre pour lisibilité */
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+        }
+        .cb-slideshow li span {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            color: transparent;
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            opacity: 0;
+            z-index: 0;
+            animation: imageAnimation 18s linear infinite 0s;
+        }
+        .cb-slideshow li:nth-child(1) span {
+            background-image: url('{{ asset("images/bg-network.png") }}');
+        }
+        .cb-slideshow li:nth-child(2) span {
+            background-image: url('{{ asset("images/bg-computer.png") }}');
+            animation-delay: 6s;
+        }
+        .cb-slideshow li:nth-child(3) span {
+            background-image: url('{{ asset("images/bg-electro.png") }}');
+            animation-delay: 12s;
+        }
+        @keyframes imageAnimation {
+            0% { opacity: 0; animation-timing-function: ease-in; transform: scale(1); }
+            8% { opacity: 1; animation-timing-function: ease-out; }
+            33% { opacity: 1; }
+            41% { opacity: 0; transform: scale(1.05); }
+            100% { opacity: 0; transform: scale(1); }
+        }
+        .login-page {
+            background: transparent !important;
+            z-index: 2;
+            position: relative;
+        }
+        .login-page::before, .login-page::after {
+            display: none !important;
+        }
+        .login-card {
+            background: rgba(255, 255, 255, 0.85); /* Légèrement transparent */
+            backdrop-filter: blur(15px);
+        }
+    </style>
 </head>
 <body>
+    <ul class="cb-slideshow">
+        <li><span></span></li>
+        <li><span></span></li>
+        <li><span></span></li>
+    </ul>
     <div class="login-page">
         <div class="login-card">
             <div class="login-logo">

28ed64007a03687a5f48c4409a48ad587345731c
Amiral <almirantevianney2005@gmail.com>
2026-04-12 10:04:38 +0100

    V10.6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         
         $middleware->trustProxies(at: '*');
+        
+        $middleware->validateCsrfTokens(except: [
+            '/login',
+            'logout'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

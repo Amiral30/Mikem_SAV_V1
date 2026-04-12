aa762015087836baff204d5c5f36cf203706358f
Amiral <almirantevianney2005@gmail.com>
2026-04-12 03:22:56 +0100

    V10.4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,10 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            if (env('APP_URL')) {
-                \Illuminate\Support\Facades\URL::forceRootUrl(env('APP_URL'));
-            }
-            
             if (!$this->app->runningInConsole()) {
                 \Illuminate\Support\Facades\URL::forceScheme('https');
             }
